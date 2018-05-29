dacefca86052dd273f62ba21126a57be42784c93
vishnukanakala <vishnu.kanakala@gmail.com>
2018-05-29 13:57:55 +0530

    test

---
+++ WebGrid_Image_Database_EF_MVC/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.IO;
+
+namespace WebGrid_Image_Database_EF_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            FilesEntities entities = new FilesEntities();
+            return View(entities.tblFiles.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            byte[] bytes;
+            using (BinaryReader br = new BinaryReader(postedFile.InputStream))
+            {
+                bytes = br.ReadBytes(postedFile.ContentLength);
+            }
+            FilesEntities entities = new FilesEntities();
+            entities.tblFiles.Add(new tblFile
+            {
+                Name = Path.GetFileName(postedFile.FileName),
+                ContentType = postedFile.ContentType,
+                Data = bytes
+            });
+            entities.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebGrid_Image_Database_EF_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebGrid_Image_Database_EF_MVC.MvcApplication" Language="C#" %>
